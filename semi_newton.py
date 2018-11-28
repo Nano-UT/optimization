8bff59b4d5f5fd95c45c554cf74382082c7cb6fc
なの <hironaga1031@yahoo.co.jp>
2018-11-28 23:23:42 +0900

    fulfilled

--- semi_newton.py
+++ semi_newton.py
@@ -1,31 +1,39 @@
-e = 0.00001
+import numpy as np
 
-xs = []
-Hs = []
-xs.append((1.2,1.2))
+a0, e, r, c1 = 1, 0.00001, 0.5, 0.5
 
 def f(x1,x2):
     return(100 * (x2 - x1 ** 2) ** 2 + (1 - x1) ** 2)
 
 def grad_k(x1,x2):
-    g_k1 = 400 * (x1 ** 3 - x1 * x2) + 2 * (x1 - 1)
-    g_k2 = 200 * (x2 - x1 ** 2)
-    return(g_k1,g_k2)
+    return(np.array([[400 * (x1 ** 3 - x1 * x2) + 2 * (x1 - 1)],[200 * (x2 - x1 ** 2)]]))
 
 def laplace_k(x1,x2):
-    l_k1 = (1200 * x1 ** 2 - 400 * x2 + 2 , -400 * x1)
-    l_k2 = (-400 * x1, 200)
-    return(l_k1,l_k2)
+    return(np.array([[1200 * x1 ** 2 - 400 * x2 + 2 , -400 * x1],[-400 * x1, 200]]))
+
+xs = []
+Hs = []
+xs.append(np.array([[1.2],[1.2]]))
+x1,x2 = xs[-1][0][0],xs[-1][1][0]
+L = laplace_k(x1,x2)
+Hs.append(np.linalg.inv(L))
 
 while(True):
-    x1,x2 = xs[-1]
+    x1,x2 = xs[-1][0][0],xs[-1][1][0]
+    H = Hs[-1]
     g = grad_k(x1,x2)
-    d = (-g[0],-g[1])
-    L = laplace_k(x1,x2)
-    if (g[0] ** 2 + g[1] ** 2) <= e ** 2:
+    if (g[0][0] ** 2 + g[1][0] ** 2) <= e ** 2:
         print("steps: " + str(len(xs)-1))
         print("optimal solution: "+str(x1) + " " + str(x2))
         break
-    det = L[0][0] * L[1][1] - L[0][1] * L [1][0]
-    d = ((d[0] * L[1][1] - d[1] * L[1][0]) / det, (-d[0] * L[0][1] + d[1] * L[0][0]) / det)
-    xs.append((x1 + d[0], x2 + d[1]))
+    d = - np.dot(H,g)
+    a = a0
+    while(True):
+        if f(x1 + a * d[0][0], x2 + a * d[1][0]) <= f(x1,x2) + c1 * a * (g[0][0]*d[0][0] + g[1][0]*d[1][0]):
+            break
+        a *= r
+    xs.append(np.array([[x1 + a * d[0][0]],[x2 + a * d[1][0]]]))
+    s = xs[-1] - xs[-2]
+    y = grad_k(xs[-1][0][0],xs[-1][1][0]) - grad_k(x1,x2)
+    H = np.dot(np.dot((np.eye(2) - np.dot(s,y.T)/np.dot(y.T,s)) , H),((np.eye(2) - np.dot(y,s.T)/np.dot(s.T,y)))) + np.dot(s,s.T) / np.dot(s.T,y)
+    Hs.append(H)
